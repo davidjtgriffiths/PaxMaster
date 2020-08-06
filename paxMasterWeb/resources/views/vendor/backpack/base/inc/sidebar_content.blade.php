184e59ef50d0fa951e8e978f6fb5dfa662608b16
David Griffiths <davidgriffiths@hotmail.com>
2020-08-06 17:18:57 +0100

    First commit

---
+++ paxMasterWeb/resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -0,0 +1,12 @@
+<!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
+
+<!-- Users, Roles Permissions -->
+  <li class="treeview">
+    <a href="#"><i class="fa fa-group"></i> <span>Users, Roles, Permissions</span> <i class="fa fa-angle-left pull-right"></i></a>
+    <ul class="treeview-menu">
+      <li><a href="{{ backpack_url('user') }}"><i class="fa fa-user"></i> <span>Users</span></a></li>
+      <li><a href="{{ backpack_url('role') }}"><i class="fa fa-group"></i> <span>Roles</span></a></li>
+      <li><a href="{{ backpack_url('permission') }}"><i class="fa fa-key"></i> <span>Permissions</span></a></li>
+    </ul>
+  </li>
